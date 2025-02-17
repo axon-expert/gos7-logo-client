84d357b5d553442595fd62168df9c41105e6dfcc
Vanik <vanixachatryan@outlook.com>
2025-02-17 10:12:47 +0300

    fix(client):  `bit` to `byte`

--- client.go
+++ client.go
@@ -142,7 +142,7 @@ func (c *client) Write(addr vmAddr, value uint32) error {
 func (c *client) writeToBuffer(addr vmAddr, buff []byte, value uint32) error {
 	switch addr.Type {
 	case Bit:
-		if err := c.client.AGReadDB(c.dbNumber, int(addr.Bit), addr.Type.Size(), buff); err != nil {
+		if err := c.client.AGReadDB(c.dbNumber, int(addr.Byte), addr.Type.Size(), buff); err != nil {
 			return err
 		}
 		if value > 0 {
